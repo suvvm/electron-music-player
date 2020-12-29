f83d8a0d1287455869c9705f5fc13c5907c3d11e
suvvm <suvvm@foxmail.com>
2020-12-29 11:55:13 +0800

    election-music-player init

--- renderer.js
+++ renderer.js
@@ -4,3 +4,20 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+
+// alert node version
+// alert(process.versions.node)
+
+const { ipcRenderer } = require('electron')
+
+window.addEventListener('DOMContentLoaded', () => {
+    // alert text
+    // alert('greeting from the DOM side')
+
+    // send msg to main process
+    ipcRenderer.send('message', 'hello from renderer')
+    ipcRenderer.on('reply', (event, args) => {
+        document.getElementById("msg").innerHTML = args;
+    })
+})
+
