4bec9092642743c05b8080bef662cbe279b66e68
suvvm <suvvm@foxmail.com>
2021-01-01 17:04:21 +0800

    add lyricContainer

--- renderer/index.js
+++ renderer/index.js
@@ -1,9 +1,16 @@
 const { ipcRenderer } = require('electron')
 const{ $ ,converDuration} = require('./helper')
+const fs = require('fs')
+
 let musicAudio = new Audio()
 let allTracks
 let currentTrack
 let musicId = 0
+let lyricVal
+let lrcHighIndex = 0
+let lrcMoveIndex = 0
+let movDis = 0
+let lyricStyle = 0
 $('add-music-button').addEventListener('click',()=>{
     ipcRenderer.send('add-music-window')
 })
@@ -20,6 +27,7 @@ jQuery(document).ready(function ($) {
                 $('#music_poster_img').attr('src',currentTrack.posterPath)
                 $('#bg_img').attr('src',currentTrack.posterPath)
             }
+
         } else {
             music = false
             $(`#tabs a[href='#home']`).tab('show')
@@ -86,6 +94,31 @@ musicAudio.addEventListener('loadedmetadata', () =>{//渲染播放器状态
 })
 
 musicAudio.addEventListener('timeupdate', ()=>{//更新播放器状态
+    var lyricContainer = $('lyricContainer')
+    console.log(musicAudio.currentTime)
+    // for (var i = 0, l = lyricVal.length; i < l; i++) {
+    //     if (musicAudio.currentTime /*当前播放的时间*/ > lyricVal[i][0]) {
+    //         //显示到页面
+    //         lyricContainer.innerHTML = lyricVal[i][1];
+    //         console.log(lyricVal[i][1])
+    //     };
+    // };
+
+    for (var i = 0, l = lyricVal.length; i < l; i++) {
+        if (musicAudio.currentTime > lyricVal[i][0] - 0.50 /*preload the lyric by 0.50s*/ ) {
+            //single line display mode
+            // that.lyricContainer.textContent = that.lyric[i][1];
+            //scroll mode
+            var line = document.getElementById('line-' + i),
+                prevLine = document.getElementById('line-' + (i > 0 ? i - 1 : i));
+            prevLine.className = '';
+            //randomize the color of the current line of the lyric
+            line.className = 'current-line-' + lyricStyle;
+            lyricContainer.style.top = 130 - line.offsetTop + 'px';
+        };
+    };
+
+
    updateProgressHTML(musicAudio.currentTime,musicAudio.duration)
 })
 
@@ -122,11 +155,16 @@ $('tracksList').addEventListener('click',(event) =>{
     const {dataset, classList} = event.target
     const id = dataset && dataset.id
     musicId = id
+
     if(id && classList.contains('fa-play')){//播放音乐
         if(currentTrack && currentTrack.id === id){//继续播放音乐
             musicAudio.play()
         }else {//播放新的歌曲，还原之前的图标
             currentTrack = allTracks.find(track => track.id === id )
+            if (currentTrack.lyricsPath) {
+                parseLyric(currentTrack.lyricsPath)
+                lyricStyle = Math.floor(Math.random() * 4)
+            }
             console.log(currentTrack.path)
             musicAudio.src = currentTrack.path
             musicAudio.play()
@@ -190,5 +228,77 @@ $('poster_div').addEventListener('click', (event) => {
         console.log(currentTrack.id)
     }
 })
+
+function appendLyric (lyric) {
+    var lyricContainer = $('lyricContainer'),
+        fragment = document.createDocumentFragment();
+    lyricContainer.style.top = '130px';
+    lyricContainer.innerHTML = '';
+    lyricVal.forEach(function(v, i, a) {
+        var line = document.createElement('p');
+        line.id = 'line-' + i;
+        line.textContent = v[1];
+        fragment.appendChild(line);
+    });
+    lyricContainer.appendChild(fragment);
+}
+
+function getOffset(text) {
+    //Returns offset in miliseconds.
+    var offset = 0;
+    try {
+        // Pattern matches [offset:1000]
+        var offsetPattern = /\[offset:\-?\+?\d+\]/g,
+            // Get only the first match.
+            offset_line = text.match(offsetPattern)[0],
+            // Get the second part of the offset.
+            offset_str = offset_line.split(':')[1];
+        // Convert it to Int.
+        offset = parseInt(offset_str);
+    } catch (err) {
+        //alert("offset error: "+err.message);
+        offset = 0;
+    }
+    return offset;
+}
+
+
+function parseLyric(path) {
+    console.log(path)
+    fs.readFile(path, (err, res) => {
+        var text = res.toString()
+        var lines = text.split('\n'),
+            //this regex mathes the time [00.12.78]
+            pattern = /\[\d{2}:\d{2}.\d{3}\]/g,
+            result = [];
+
+        // Get offset from lyrics
+        var offset = getOffset(text);
+
+        //exclude the description parts or empty parts of the lyric
+        while (!pattern.test(lines[0])) {
+            lines = lines.slice(1);
+        };
+        //remove the last empty item
+        lines[lines.length - 1].length === 0 && lines.pop();
+        //display all content on the page
+        lines.forEach(function(v, i, a) {
+            var time = v.match(pattern),
+                value = v.replace(pattern, '');
+            time.forEach(function(v1, i1, a1) {
+                //convert the [min:sec] to secs format then store into result
+                var t = v1.slice(1, -1).split(':');
+                result.push([parseInt(t[0], 10) * 60 + parseFloat(t[1]) + parseInt(offset) / 1000, value]);
+            });
+        });
+        //sort the result by time
+        result.sort(function(a, b) {
+            return a[0] - b[0];
+        });
+        console.log(result);
+        lyricVal = result
+        appendLyric(lyricVal);
+    })
+}
 
 // var width = $('player-progress').offsetHeight
